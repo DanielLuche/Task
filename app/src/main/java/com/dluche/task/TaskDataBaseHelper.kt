9df8ba14b5b22eac0546fcc3eb8d288329459077
DanielLuche <d.luche@namoadigital.com>
2018-11-15 13:47:00 -0200

    Inicio aula Db 1

---
+++ app/src/main/java/com/dluche/task/TaskDataBaseHelper.kt
@@ -0,0 +1,11 @@
+package com.dluche.task
+
+class TaskDataBaseHelper {
+
+    //SQLite
+    //Tipos de dados do SQLite
+    //INTEGER, REAL(DOUBLE), TEXT , BLOB
+
+    private val createTableUser = "create table user (name text, id integer)"
+
+}
