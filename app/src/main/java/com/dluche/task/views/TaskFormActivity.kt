4baaa3152234e6fea8675ebdfcde77b98ed2a3dd
DanielLuche <d.luche@namoadigital.com>
2018-11-24 13:32:23 -0200

    Add new files Até inicio da aula 169, criação da entide Prioridade.

---
+++ app/src/main/java/com/dluche/task/views/TaskFormActivity.kt
@@ -0,0 +1,13 @@
+package com.dluche.task.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dluche.task.R
+
+class TaskFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_form)
+    }
+}
